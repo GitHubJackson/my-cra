4b783ba6f47a0d4fbf4b4d338e7faa2b39d8f8bf
周伟滨 <lucas@zhouweibindeMacBook-Pro-2.local>
2024-02-17 17:32:37 +0800

    initial project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const root = document.getElementById("root");
+console.log("===root", root);
+if (root) {
+  root.textContent = "hello my-cra";
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8000,
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
